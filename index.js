afdddefae59573a001bd781c502312b17b3fe36a
sabih <sabih.dev@gmail.com>
2025-10-27 13:51:47 +0500

    added the yamlfile

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log('hello i am learning ci/cd pipelines');
+console.log('this is my first pipeline');
+console.log('i am excited to automate my deployments');
+console.log('ci/cd is fun!');
---
+++ test.js
@@ -0,0 +1,6 @@
+console.log('test will start in 3 second ......');
+setTimeout(()=>{
+    console.log('waiting 3 seconds...');
+}, 3000);
+console.log('test finished');
+
